8f97a11350f856e71fa5879c88872486548e8a2f
BubblyBerty <gath_music@yahoo.co.uk>
2024-03-20 01:54:43 +0000

    Nearly completed Scales-Modes page

---
+++ Tasks-Assignments/GathPortfolio/Gath_Website/gath_Home_DOM.js
@@ -0,0 +1,51 @@
+// Gathering the buttons
+let button1 = document.getElementById('button1')
+let button2 = document.getElementById('button2')
+let button3 = document.getElementById('button3')
+let button4 = document.getElementById('button4')
+let button5 = document.getElementById('button5')
+let button6 = document.getElementById('button6')
+
+// Creating the Event and Event Listener
+// (event type, function for each button)
+button1.addEventListener('mouseover', function run(){
+    button1.style.backgroundColor = 'orange'
+})
+button1.addEventListener('mouseleave', function run(){
+    button1.style.backgroundColor = 'aqua'
+})
+
+button2.addEventListener('mouseover', function run(){
+    button2.style.backgroundColor = 'orange'
+})
+button2.addEventListener('mouseleave', function run(){
+    button2.style.backgroundColor = 'aqua'
+})
+
+button3.addEventListener('mouseover', function run(){
+    button3.style.backgroundColor = 'orange'
+})
+button3.addEventListener('mouseleave', function run(){
+    button3.style.backgroundColor = 'aqua'
+})
+
+button4.addEventListener('mouseover', function run(){
+    button4.style.backgroundColor = 'orange'
+})
+button4.addEventListener('mouseleave', function run(){
+    button4.style.backgroundColor = 'aqua'
+})
+
+button5.addEventListener('mouseover', function run(){
+    button5.style.backgroundColor = 'orange'
+})
+button5.addEventListener('mouseleave', function run(){
+    button5.style.backgroundColor = 'aqua'
+})
+
+button6.addEventListener('mouseover', function run(){
+    button6.style.backgroundColor = 'orange'
+})
+button6.addEventListener('mouseleave', function run(){
+    button6.style.backgroundColor = 'aqua'
+})
