27f8a88edd70eba654a3efbfdab6b3f4e17f7d07
BubblyBerty <gath_music@yahoo.co.uk>
2024-03-22 14:01:04 +0000

    Calculator displayed

--- Tasks-Assignments/GathPortfolio/Gath_Website/gath_Calculator_DOM.js
+++ Tasks-Assignments/GathPortfolio/Gath_Website/gath_Calculator_DOM.js
@@ -1,3 +1,54 @@
+// Gathering the buttons
+let buttonMenu1 = document.getElementById('buttonMenu1')
+let buttonMenu2 = document.getElementById('buttonMenu2')
+let buttonMenu3 = document.getElementById('buttonMenu3')
+let buttonMenu4 = document.getElementById('buttonMenu4')
+let buttonMenu5 = document.getElementById('buttonMenu5')
+let buttonMenu6 = document.getElementById('buttonMenu6')
+
+buttonMenu1.addEventListener('mouseover', function run(){
+  buttonMenu1.style.backgroundColor = 'orange'
+})
+buttonMenu1.addEventListener('mouseleave', function run(){
+  buttonMenu1.style.backgroundColor = 'aqua'
+})
+
+buttonMenu2.addEventListener('mouseover', function run(){
+  buttonMenu2.style.backgroundColor = 'orange'
+})
+buttonMenu2.addEventListener('mouseleave', function run(){
+  buttonMenu2.style.backgroundColor = 'aqua'
+})
+
+buttonMenu3.addEventListener('mouseover', function run(){
+  buttonMenu3.style.backgroundColor = 'orange'
+})
+buttonMenu3.addEventListener('mouseleave', function run(){
+  buttonMenu3.style.backgroundColor = 'aqua'
+})
+
+buttonMenu4.addEventListener('mouseover', function run(){
+  buttonMenu4.style.backgroundColor = 'orange'
+})
+buttonMenu4.addEventListener('mouseleave', function run(){
+  buttonMenu4.style.backgroundColor = 'aqua'
+})
+
+buttonMenu5.addEventListener('mouseover', function run(){
+  buttonMenu5.style.backgroundColor = 'orange'
+})
+buttonMenu5.addEventListener('mouseleave', function run(){
+  buttonMenu5.style.backgroundColor = 'aqua'
+})
+
+buttonMenu6.addEventListener('mouseover', function run(){
+  buttonMenu6.style.backgroundColor = 'orange'
+})
+buttonMenu6.addEventListener('mouseleave', function run(){
+  buttonMenu6.style.backgroundColor = 'aqua'
+})
+
+
 let buTT = document.getElementsByClassName("button1");
 let buTTS = document.getElementById("search");
 
