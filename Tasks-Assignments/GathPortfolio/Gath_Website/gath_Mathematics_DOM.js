2390454c2e1240ff2d18acfa120b4c33e04f5267
BubblyBerty <gath_music@yahoo.co.uk>
2024-03-26 23:42:35 +0000

    Added maths

---
+++ Tasks-Assignments/GathPortfolio/Gath_Website/gath_Mathematics_DOM.js
@@ -0,0 +1,56 @@
+// Gathering the buttons
+let buttonMenu1 = document.getElementById('buttonMenu1')
+let buttonMenu2 = document.getElementById('buttonMenu2')
+let buttonMenu3 = document.getElementById('buttonMenu3')
+let buttonMenu4 = document.getElementById('buttonMenu4')
+let buttonMenu5 = document.getElementById('buttonMenu5')
+let buttonMenu6 = document.getElementById('buttonMenu6')
+
+// Creating the Event and Event Listener
+// (event type, function for each button) (I tried to use this method with classes but it only appears to work for the first instance)
+buttonMenu1.addEventListener('mouseover', function run(){
+    buttonMenu1.style.backgroundColor = 'orange'
+})
+buttonMenu1.addEventListener('mouseleave', function run(){
+    buttonMenu1.style.backgroundColor = 'aqua'
+})
+
+buttonMenu2.addEventListener('mouseover', function run(){
+    buttonMenu2.style.backgroundColor = 'orange'
+})
+buttonMenu2.addEventListener('mouseleave', function run(){
+    buttonMenu2.style.backgroundColor = 'aqua'
+})
+
+buttonMenu3.addEventListener('mouseover', function run(){
+    buttonMenu3.style.backgroundColor = 'orange'
+})
+buttonMenu3.addEventListener('mouseleave', function run(){
+    buttonMenu3.style.backgroundColor = 'aqua'
+})
+
+buttonMenu4.addEventListener('mouseover', function run(){
+    buttonMenu4.style.backgroundColor = 'orange'
+})
+buttonMenu4.addEventListener('mouseleave', function run(){
+    buttonMenu4.style.backgroundColor = 'aqua'
+})
+
+buttonMenu5.addEventListener('mouseover', function run(){
+    buttonMenu5.style.backgroundColor = 'orange'
+})
+buttonMenu5.addEventListener('mouseleave', function run(){
+    buttonMenu5.style.backgroundColor = 'aqua'
+})
+
+buttonMenu6.addEventListener('mouseover', function run(){
+    buttonMenu6.style.backgroundColor = 'orange'
+})
+buttonMenu6.addEventListener('mouseleave', function run(){
+    buttonMenu6.style.backgroundColor = 'aqua'
+})
+
+document.addEventListener('DOMContentLoaded', function() {
+    var audio = document.querySelector('audio');
+    audio.removeAttribute('controls');
+});
