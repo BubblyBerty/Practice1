75d2f062ee1c2fcffc05df3e45e08befe4753421
BubblyBerty <gath_music@yahoo.co.uk>
2024-04-29 16:35:53 +0100

    Updating GitHub for my website link

---
+++ Tasks-Assignments/GathPortfolio/Gath_Website/gath_Me_DOM.js
@@ -0,0 +1,83 @@
+// Gathering the buttons
+let button1 = document.getElementById('button1')
+let button2 = document.getElementById('button2')
+let button3 = document.getElementById('button3')
+let button4 = document.getElementById('button4')
+let button5 = document.getElementById('button5')
+let button6 = document.getElementById('button6')
+let button7 = document.getElementById('button7')
+let button8 = document.getElementById('button8')
+let button9 = document.getElementById('button9')
+let button10 = document.getElementById('button10')
+
+// Creating the Event and Event Listener
+// (event type, function for each button)
+button1.addEventListener('mouseover', function run(){
+    button1.style.backgroundColor = 'orange'
+})
+button1.addEventListener('mouseleave', function run(){
+    button1.style.backgroundColor = 'aqua'
+})
+
+button2.addEventListener('mouseover', function run(){
+    button2.style.backgroundColor = 'orange'
+})
+button2.addEventListener('mouseleave', function run(){
+    button2.style.backgroundColor = 'aqua'
+})
+
+button3.addEventListener('mouseover', function run(){
+    button3.style.backgroundColor = 'orange'
+})
+button3.addEventListener('mouseleave', function run(){
+    button3.style.backgroundColor = 'aqua'
+})
+
+button4.addEventListener('mouseover', function run(){
+    button4.style.backgroundColor = 'orange'
+})
+button4.addEventListener('mouseleave', function run(){
+    button4.style.backgroundColor = 'aqua'
+})
+
+button5.addEventListener('mouseover', function run(){
+    button5.style.backgroundColor = 'orange'
+})
+button5.addEventListener('mouseleave', function run(){
+    button5.style.backgroundColor = 'aqua'
+})
+
+button6.addEventListener('mouseover', function run(){
+    button6.style.backgroundColor = 'orange'
+})
+button6.addEventListener('mouseleave', function run(){
+    button6.style.backgroundColor = 'aqua'
+})
+
+button7.addEventListener('mouseover', function run(){
+    button7.style.backgroundColor = 'orange'
+})
+button7.addEventListener('mouseleave', function run(){
+    button7.style.backgroundColor = 'aqua'
+})
+
+button8.addEventListener('mouseover', function run(){
+    button8.style.backgroundColor = 'orange'
+})
+button8.addEventListener('mouseleave', function run(){
+    button8.style.backgroundColor = 'aqua'
+})
+
+button9.addEventListener('mouseover', function run(){
+    button9.style.backgroundColor = 'orange'
+})
+button9.addEventListener('mouseleave', function run(){
+    button9.style.backgroundColor = 'aqua'
+})
+
+button10.addEventListener('mouseover', function run(){
+    button10.style.backgroundColor = 'orange'
+})
+button10.addEventListener('mouseleave', function run(){
+    button10.style.backgroundColor = 'aqua'
+})
